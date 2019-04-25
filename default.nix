a1c4a9c089bc563a4da8b03e0e4d9ba17669e952
Alexandre Peyroux <alex@px.io>
2019-04-25 11:13:31 +0200

    use writeHaskellbin

--- default.nix
+++ default.nix
@@ -1,5 +1,14 @@
 with import <nixpkgs> {};
 
-writeScriptBin "bkp" ''
-  ${restic}/bin/restic -r b2:px-restic:xps15 --exclude={*.iso,.cabal,.cache,.cargo,.go,$HOME/tmp,$HOME/dwl,$HOME/vbox} backup /home/alex/
-  ''
+writers.writeHaskellBin "bkp" {libraries=[haskellPackages.shelly];} ''
+{-# LANGUAGE OverloadedStrings #-}
+
+import Shelly
+
+main = shelly $
+  run "${restic}/bin/restic" ["-r"
+                            , "b2:px-restic:xps15" 
+                            , "--exclude={*.iso,.cabal,.cache,.cargo,.go,$HOME/tmp,$HOME/dwl,$HOME/vbox}" 
+                            , "backup" 
+                            , "/home/alex/"]
+''
