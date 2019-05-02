f2b10e5d6c8c2ea140234dc05d871ce98c51daab
Alexandre Peyroux <alex@px.io>
2019-05-02 20:44:42 +0200

    use sh

--- default.nix
+++ default.nix
@@ -1,14 +1,18 @@
 with import <nixpkgs> {};
 
-writers.writeHaskellBin "bkp" {libraries=[haskellPackages.shelly];} ''
-{-# LANGUAGE OverloadedStrings #-}
+# writers.writeHaskellBin "bkp" {libraries=[haskellPackages.shelly];} ''
+# {-# LANGUAGE OverloadedStrings #-}
 
-import Shelly
+# import Shelly
 
-main = shelly $
-  run "${restic}/bin/restic" ["-r"
-                            , "b2:px-restic:xps15" 
-                            , "--exclude={*.iso,.cabal,.cache,.cargo,.go,$HOME/tmp,$HOME/dwl,$HOME/vbox}" 
-                            , "backup" 
-                            , "/home/alex/"]
+# main = shelly $
+#   run "${restic}/bin/restic" ["-r"
+#                             , "b2:px-restic:xps15" 
+#                             , "--exclude={*.iso,.cabal,.cache,.cargo,.go,$HOME/tmp,$HOME/dwl,$HOME/vbox}" 
+#                             , "backup" 
+#                             , "/home/alex/"]
+# ''
+
+writeScriptBin "bkp" ''
+${restic}/bin/restic -r b2:px-restic:xps15 --exclude={*.iso,.cabal,.cache,.cargo,.go,$HOME/tmp,$HOME/dwl,$HOME/vbox} backup /home/alex/
 ''
